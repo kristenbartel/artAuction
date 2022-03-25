2a736e30332b7d1b491016969ac527bf2fe2a3cb
Kristen Bartel <kristenbartel@gmail.com>
2022-03-24 21:19:36 -0500

    commit

--- config/config.js
+++ config/config.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD:config/config.json
-{
-  "production": {
-    "username": "ohjyjzop",
-    "password": "43MXSywJ3HrYbdlxEEvVCVItnLDU-J1G",
-    "database": "ohjyjzop",
-    "host": "kashin.db.elephantsql.com",
-=======
 require('dotenv').config();
 module.exports = {
   "development": {
@@ -13,9 +5,6 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
->>>>>>> 4a3388fe7686a699a6e57f78315b411d8a4723c9:config/config.js
     "dialect": "postgres"
   }
 }
-
-
